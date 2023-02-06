fe675efc145311f483ba606b54d95ee92de93518
il94 <ilyes.landolsi@outlook.fr>
2023-02-06 13:03:11 +0100

    module 08 ex02 1%

---
+++ cpp08/ex02/include/MutantStack.hpp
@@ -0,0 +1,31 @@
+#ifndef MUTANTSTACK
+#define MUTANTSTACK
+
+#include <iostream>
+
+class MutantStack
+{
+	public :
+
+	/* Constructors */
+	MutantStack();
+	MutantStack(const MutantStack&);
+	~MutantStack();
+
+	/* Overloads */
+	MutantStack&	operator=(const MutantStack&);
+
+	/* Methods */
+	
+
+	/* Accessors */
+	
+
+	private :
+
+	/* Attributes */
+	
+
+};
+
+#endif
---
+++ cpp08/ex02/src/MutantStack.cpp
@@ -0,0 +1,27 @@
+#include "../include/MutantStack.hpp"
+
+/*=============================== Constructors ===============================*/
+
+MutantStack::MutantStack(){
+}
+
+MutantStack::MutantStack(const MutantStack &src){
+	*this = src;
+}
+
+MutantStack::~MutantStack(){
+}
+
+/*================================ Overloads =================================*/
+
+MutantStack& MutantStack::operator=(const MutantStack &src)
+{
+}
+
+/*================================= Methods ==================================*/
+
+
+
+/*================================ Accessors =================================*/
+
+
