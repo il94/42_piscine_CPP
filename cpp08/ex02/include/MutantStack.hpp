66dff0786dd8ee8bdf0973ea45c1ecaabd77dc04
il94 <ilyes.landolsi@outlook.fr>
2023-02-06 19:39:08 +0100

    module 08 OK

--- cpp08/ex02/include/MutantStack.hpp
+++ cpp08/ex02/include/MutantStack.hpp
@@ -2,30 +2,110 @@
 #define MUTANTSTACK
 
 #include <iostream>
+#include <stack>
+#include <algorithm>
 
-class MutantStack
+template <typename T>
+void	printElement(T i){
+	std::cout << "[" << i << "] ";
+}
+
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 	public :
 
+	class iterator
+	{
+		public:
+
+		/* Constructors */
+		iterator(){
+		}
+
+		iterator(typename std::deque<T>::iterator src) : _value(src){
+		}
+
+		iterator(iterator const& src) : _value(src._value){
+		}
+
+		~iterator(){
+		}
+
+
+		/* Overloads */
+		iterator&	operator=(const iterator& src){
+			_value = src._value;
+			return (*this);
+		}
+		
+		T 		operator*( void ){
+			return (*_value);
+		}
+
+		iterator	operator+(const int &factor) const
+		{
+			iterator	result = *this;
+
+			result._value += factor;
+			return (result);
+		}
+
+		iterator	operator-(const int &factor) const
+		{
+			iterator	result = *this;
+
+			result._value -= factor;
+			return (result);
+		}
+
+
+		bool		operator==(const iterator& target) const{
+			return (_value == target._value);
+		}
+		bool		operator!=(const iterator& target) const{
+			return (_value != target._value);
+		}
+
+		iterator& 	operator++( void ){
+			_value++;
+			return (*this);
+		}
+		iterator& 	operator--( void ){
+			_value--;
+			return (*this);
+		}
+
+		private:
+		
+		/* Attributes */
+		typename std::deque<T>::iterator	_value;
+
+	};
+
 	/* Constructors */
-	MutantStack();
-	MutantStack(const MutantStack&);
-	~MutantStack();
+	MutantStack() : std::stack<T>(){
+	}
+
+	MutantStack(const MutantStack& src) : std::stack<T>(src){
+		*this = src;
+	}
+
+	~MutantStack(){
+	}
+
 
 	/* Overloads */
-	MutantStack&	operator=(const MutantStack&);
+	using std::stack<T>::operator=;
 
-	/* Methods */
-	
+	/* Iterators */
+	MutantStack::iterator	begin( void ){
+		return (iterator(this->c.begin()));
+	}
 
-	/* Accessors */
-	
-
-	private :
-
-	/* Attributes */
-	
-
+	MutantStack::iterator	end( void ){
+		return (iterator(this->c.end()));
+	}	
 };
 
 #endif
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,35 +1,136 @@
 #include "../include/MutantStack.hpp"
-#define SIZE_MAX 50
+
+// int main( void )
+// {
+// 	MutantStack<int> mstack;
+
+// 	mstack.push(5);
+// 	mstack.push(17);
+
+// 	std::cout << mstack.top() << std::endl;
+
+// 	mstack.pop();
+
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push(3);
+// 	mstack.push(5);
+// 	mstack.push(737);
+// 	//[...]
+// 	mstack.push(0);
+
+// 	MutantStack<int>::iterator it = mstack.begin();
+// 	MutantStack<int>::iterator ite = mstack.end();
+
+// 	++it;
+// 	--it;
+// 	while (it != ite)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+// 	std::stack<int> s(mstack);
+// 	return 0;
+// }
 
 int main( void )
 {
-	MutantStack<int> mstack;
+	MutantStack<int> myStack;
+	myStack.push(0); printElement(myStack.top());
+	myStack.push(10); printElement(myStack.top());
+	myStack.push(50); printElement(myStack.top());
+	myStack.push(150); printElement(myStack.top());
+	std::cout << std::endl;
 
-	mstack.push(5);
-	mstack.push(17);
+	std::cout << std::endl;
+	std::cout << "===================================" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "My Incredible Stack top is : " << myStack.top() << std::endl;
+	std::cout << std::endl;
 
-	mstack.pop();
+	MutantStack<int> myCopyStack(myStack);
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << "My Awesome Copy Stack top is : " << myCopyStack.top() << std::endl;
+	std::cout << "[Pop -> new]" << std::endl;
+	myCopyStack.pop();
+	std::cout << "My Awesome Copy Stack top is : " << myCopyStack.top() << std::endl;
 
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
+	std::cout << std::endl;
+	std::cout << "===================================" << std::endl;
+	std::cout << std::endl;
 
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << "My Fabulous Stack (displayed by my fantastic iterators) is : ";
+	std::for_each(myStack.begin(), myStack.end(), printElement<int>);
+	std::cout << std::endl;	
 
-	++it;
-	--it;
-	while (it != ite)
+	std::stack<int> original(myStack);
+	std::cout << "std::stack top is : " << original.top() << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "===================================" << std::endl;
+	std::cout << std::endl;
+
+	MutantStack<std::string> myStringStack;
+
+	myStringStack.push("Ca");
+	myStringStack.push("marche");
+	myStringStack.push("aussi");
+	myStringStack.push("avec");
+	myStringStack.push("des");
+	myStringStack.push("strings");
+	myStringStack.push("!!!");
+
+	std::for_each(myStringStack.begin(), myStringStack.end(), printElement<std::string>);
+	std::cout << std::endl;
+
+	for (MutantStack<std::string>::iterator it = myStringStack.begin(); it != myStringStack.end(); ++it)
 	{
-		std::cout << *it << std::endl;
-		++it;
+		if (it == myStringStack.begin() or it == myStringStack.begin() + 1 or it == myStringStack.end() - 1)
+			printElement<std::string>(*it);
 	}
-	std::stack<int> s(mstack);
-	return 0;
+	std::cout << std::endl;
+
+	return (0);
 }
+
+// #include <list>
+// int main( void )
+// {
+// 	std::list<int> myStack;
+// 	myStack.push_back(0); printElement(myStack.back());
+// 	myStack.push_back(10); printElement(myStack.back());
+// 	myStack.push_back(50); printElement(myStack.back());
+// 	myStack.push_back(150); printElement(myStack.back());
+// 	std::cout << std::endl;
+
+// 	std::cout << std::endl;
+// 	std::cout << "===================================" << std::endl;
+// 	std::cout << std::endl;
+
+// 	std::cout << "My Incredible Stack top is : " << myStack.back() << std::endl;
+// 	std::cout << std::endl;
+
+// 	std::list<int> myCopyStack(myStack);
+
+// 	std::cout << "My Awesome Copy Stack top is : " << myCopyStack.back() << std::endl;
+// 	std::cout << "[Pop -> new]" << std::endl;
+// 	myCopyStack.pop_back();
+// 	std::cout << "My Awesome Copy Stack top is : " << myCopyStack.back() << std::endl;
+
+// 	std::cout << std::endl;
+// 	std::cout << "===================================" << std::endl;
+// 	std::cout << std::endl;
+
+
+// 	std::cout << "My Fabulous Stack is : ";
+// 	for (std::list<int>::iterator it = myStack.begin(); it != myStack.end(); ++it)
+// 		printElement(*it);
+// 	std::cout << std::endl;
+
+
+// 	std::list<int> original(myStack);
+// 	std::cout << "std::stack back is : " << original.back() << std::endl;
+
+// 	return 0;
+// }
