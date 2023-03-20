75c7a53cd39df2500910a26bbd00e22e2e5f864f
il94 <ilyes.landolsi@outlook.fr>
2023-03-20 21:36:41 +0100

    module 09 ex01 75%

--- cpp09/ex01/src/RPN.cpp
+++ cpp09/ex01/src/RPN.cpp
@@ -26,15 +26,49 @@ RPN& RPN::operator=(const RPN &src)
 
 long long	RPN::applyRPN( void )
 {
-	// long long	result = 0;
+	long long	result = 0;
 	// int			tmp;
+
+	result = _values.top();
+	_values.pop();
+
+	while (not _values.empty())
+	{
+		std::cout << "RES " << result << std::endl;
+		std::cout << "VAL " << _values.top() << std::endl;
+		std::cout << "OP "<< _operators.top() << std::endl;
+		std::cout << "=================" << std::endl << std::endl;
+
+
+		switch (_operators.top())
+		{
+			case '+' :
+				result += _values.top();
+				break ;
+			case '-' :
+				result -= _values.top();
+				break ;
+			case '*' :
+				result *= _values.top();
+				break ;
+			case '/' :
+				result /= _values.top();
+				break ;
+			default :
+				std::cout << "RAYANES" << std::endl;
+		}
+		_operators.pop();
+		_values.pop();
+	}
+	
+	// std::cout << "_values.top()" << std::endl;
 
 	// tmp = _values.top();
 	// _values.pop();
 	// result = 
 
 
-	// result = 
+	_result = result;
 	return (getResult());
 }
 
